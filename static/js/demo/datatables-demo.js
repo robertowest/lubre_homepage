073ad759c11cf07df32f743c9ca983e794026b19
robertowest <roberto.west@gmail.com>
2020-08-25 16:41:27 -0300

    modificaciones en casa

--- static/js/demo/datatables-demo.js
+++ static/js/demo/datatables-demo.js
@@ -27,6 +27,35 @@ $(document).ready(function() {
       }
     },
     "lengthMenu": [ 10, 15, 20, 50, 100 ],
+    "recordsFiltered": 100, 
+  });
+  
+  $('#dataTableCondensed').DataTable({
+    "language": {
+      "decimal":        "",
+      "emptyTable":     "No hay datos disponibles en la tabla",
+      "info":           "Mostrando _START_ a _END_ de _TOTAL_ registros",
+      "infoEmpty":      "No hay registros disponibles",
+      "infoFiltered":   "(filtrado de _MAX_ registros)",
+      "infoPostFix":    "",
+      "thousands":      ",",
+      "lengthMenu":     "Mostrar _MENU_ registros",
+      "loadingRecords": "Cargando ...",
+      "processing":     "Procesando ...",
+      "search":         "Buscar:",
+      "zeroRecords":    "No se encontraron registros",
+      "paginate": {
+          "first":      "Primero",
+          "last":       "Último",
+          "next":       "Siguiente",
+          "previous":   "Anterior"
+      },
+      "aria": {
+          "sortAscending":  ": activar para ordenar la columna ascendente",
+          "sortDescending": ": activar para ordenar la columna descendente"
+      }
+    },
+    "lengthMenu": [ 10, 15, 20, 50, 100 ],
     "ordering": false,
     "pageLength": 15,
     "recordsTotal": 100,
