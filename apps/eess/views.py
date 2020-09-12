cf207fa57086ae09cec479ee53796b364ff8cc6b
robertowest <roberto.west@gmail.com>
2020-09-12 19:39:50 -0300

    modificaciones en casa

--- apps/eess/views.py
+++ apps/eess/views.py
@@ -4,8 +4,12 @@ from django.views import generic
 from . import models
 
 
+# class PDFTemplateView(generic.TemplateView):
+#     def get(self, request, *args, **kwargs):
+#         return CartillaTemplateView.as_view()(request)
+
+
 class CartillaTemplateView(generic.TemplateView):
-    # model = models.Cartilla
     template_name = 'cartilla/index.html'
 
 
@@ -36,19 +40,6 @@ class CombosTemplateView(generic.TemplateView):
         return context
 
 
-# class KioskoTemplateView(generic.TemplateView):
-#     model = models.Cartilla
-#     template_name = 'cartilla/recuadros.html'
-
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         object_list = [
-#             models.Cartilla.objects.filter(grupo='bebida').filter(active=True).order_by('id'),
-#             models.Cartilla.objects.filter(grupo='kiosco').filter(active=True).order_by('id'),
-#             models.Cartilla.objects.filter(grupo='snak').filter(active=True).order_by('id'),
-#         ]
-#         context['object_list'] = object_list
-#         return context
 class KioskoTemplateView(generic.TemplateView):
     model = models.Cartilla
     template_name = 'cartilla/listado.html'
