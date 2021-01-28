27f4aa33bb19fa88386cf4457d0c544745294682
robertowest <roberto.west@gmail.com>
2021-01-28 09:53:02 -0300

    modificaciones en casa

--- push.sh
+++ push.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 git add .
-git add -f requirements.txt
 
 # quitamos archivos que no queremos modificar
-# git reset config/desarrollo.py
+# git reset config/settings.py
 
 git commit -m "modificaciones en casa"
 git push -u origin master
@@ -23,3 +22,16 @@ git push -u origin master
 # Limpiando archivos ignorados
 # git clean -Xn      - Vista previa de todos los archivos que serán limpiados
 # git clean -Xf      - Se eliminarán todos los archivos ignorados del directorio actual y todos los subdirectorios
+
+# agregar archivos al listado de ignorados (si ya fueron subidos)
+# por ejemplo: quiero ignorar el archivo settings.ini que ya fue sincronizado al repositorio
+# 1. eliminamos la entrada del cache
+#    git rm --cached config/settings.ini
+# 2. lo agregamos al archivo .gitignore
+#    echo 'config/settings.ini' >> .gitignore
+# 3. realize un commit y push
+#    git commit -m "actuaización de archivos ignorados"
+#    git push
+
+# si queremos agregar un archivo que fue ignorado al control
+# git add -f requirements.txt
