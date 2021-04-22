9b2f1c3608e0a0382f0b57293d2621fd0411c387
robertowest <roberto.west@gmail.com>
2021-04-22 16:49:25 -0300

    modificaciones en el trabajo

--- config/urls.py
+++ config/urls.py
@@ -13,11 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from django.views import defaults as default_views
 from django.views.generic import TemplateView
 
 from apps.accounts.views import LoginRedirect
@@ -75,6 +72,10 @@ urlpatterns = [
     # MP -----------------------------------------------------------------------------
     path('mercadopago/', include('django_mercadopago.urls')),
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views import defaults as default_views
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
