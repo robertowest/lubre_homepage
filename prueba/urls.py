162abcf3a6914a84496bb0d40660657714604807
Roberto West <roberto.west@gmail.com>
2021-04-23 15:28:06 -0300

    modificaciones en el trabajo

--- prueba/urls.py
+++ prueba/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
 
     path('ajax_cargar_filtro/', views.ajax_cargar_filtro, name='ajax_cargar_filtro'),
     path('ajax_cargar_tabla/', views.ajax_cargar_tabla, name='ajax_cargar_tabla'),
+    path('ajax_asociar_elementos/', views.ajax_asociar_elementos, name='ajax_asociar_elementos'),
 ]
--- prueba/views.py
+++ prueba/views.py
@@ -1,7 +1,10 @@
+import json
+
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.db.models import Q
 from django.http import HttpResponse, HttpResponseRedirect, QueryDict
+from django.http.response import JsonResponse
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.views import generic
@@ -14,23 +17,19 @@ from django_tables2.paginators import LazyPaginator
 from apps.persona import filters, forms, models, tables
 from apps.empresa.models import Comercial
 
-from apps.comunes.models import Comunicacion as ComunicacionModel
-from apps.comunes.models import Domicilio as DomicilioModel
-from apps.comunes.forms.comunicacion import ComunicacionForm
-from apps.comunes.forms.domicilio import DomicilioForm
 from apps.comunes.utils import PagedFilteredTableView
 
 # asociar persona con comunicación
 from apps.comunes.models import Comunicacion
 from apps.comunes.filters import ComunicacionFindFilter
 from apps.comunes.tables import ComunicacionFindTable
-from apps.comunes.forms.comunicacion import ComunicacionFilterFormModal
+from apps.comunes.forms.comunicacion import ComunicacionForm, ComunicacionFilterFormModal
 
 # asociar persona con domicilio
 from apps.comunes.models import Domicilio
 from apps.comunes.filters import DomicilioFindFilter
 from apps.comunes.tables import DomicilioFindTable
-from apps.comunes.forms.domicilio import DomicilioFilterFormModal
+from apps.comunes.forms.domicilio import DomicilioForm, DomicilioFilterFormModal
 
 
 class PersonaDetailView(PermissionRequiredMixin, generic.DetailView):
@@ -54,7 +53,6 @@ def ajax_cargar_filtro(request):
         filter.form.helper = DomicilioFilterFormModal()
     return render(request, 'prueba/includes/_modal_find_form.html', { 'filter': filter })
 
-
 def ajax_cargar_tabla(request):
     if request.GET['app'] == 'comunicacion':
         filter = ComunicacionFindFilter(request.GET, queryset=Comunicacion.objects.all())
@@ -66,3 +64,16 @@ def ajax_cargar_tabla(request):
         table = DomicilioFindTable(filter.qs[:5])   # solo 10 registros
     RequestConfig(request).configure(table)
     return render(request, 'prueba/includes/_modal_find_table.html', { 'table': table })
+
+def ajax_asociar_elementos(request):
+    if request.GET['app'] == 'comunicacion':
+        persona = models.Persona.objects.get(id=request.GET['pk'])
+        comunicacion = Comunicacion.objects.get(id=request.GET['fk'])
+        persona.comunicaciones.add(comunicacion)
+        persona.save()
+    elif request.GET['app'] == 'domicilio':
+        persona = models.Persona.objects.get(id=request.GET['pk'])
+        persona.domicilio = Domicilio.objects.get(id=request.GET['fk'])
+        persona.save()
+    # return JsonResponse({'result': True}, status=200)
+    return HttpResponseRedirect(reverse('prueba:detail', args=[persona.pk]))
