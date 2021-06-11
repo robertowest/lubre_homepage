670d346efba100982a991ecad1429208719ceb0f
Roberto West <roberto.west@gmail.com>
2021-06-11 12:04:55 -0300

    copia interna

--- apps/landing/static/js/filtro.js
+++ apps/landing/static/js/filtro.js
@@ -1,35 +1,32 @@
 $(document).ready(function() {
-
-    // AGREGANDO CLASE ACTIVE AL PRIMER ENLACE ====================
+    // agregamos la clase active al primer enlace
     $('.category_list .category_item[category="all"]').addClass('ct_item-active');
 
-    // FILTRANDO PRODUCTOS  ============================================
-
+    // filtro de productos
     $('.category_item').click(function(){
         var catProduct = $(this).attr('category');
         console.log(catProduct);
 
         alert(catProduct); 
 
-        // AGREGANDO CLASE ACTIVE AL ENLACE SELECCIONADO
+        // agregamos la clase active al enlace seleccionado
         $('.category_item').removeClass('ct_item-active');
         $(this).addClass('ct_item-active');
 
-        // OCULTANDO PRODUCTOS =========================
+        // ocultamos los productos de otras categorías (grupos)
         $('.product-item').css('transform', 'scale(0)');
         function hideProduct(){
             $('.product-item').hide();
         } setTimeout(hideProduct,400);
 
-        // MOSTRANDO PRODUCTOS =========================
+        // visualizamos los productos de la categoría (grupo) seleccionada
         function showProduct(){
             $('.product-item[category="'+catProduct+'"]').show();
             $('.product-item[category="'+catProduct+'"]').css('transform', 'scale(1)');
         } setTimeout(showProduct,400);
     });
 
-    // MOSTRANDO TODOS LOS PRODUCTOS =======================
-
+    // mostramos todos los productos
     $('.category_item[category="all"]').click(function(){
         function showAll(){
             $('.product-item').show();
